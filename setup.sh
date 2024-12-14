c97762e7323230a4c5eb7c44a11cd9d6c46d2f9a
Sibbe Bakker <sibbe.bakker@wur.nl>
2024-12-14 14:48:22 +0100

    add linux startup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
-R -q -e " remotes::install_github('extendr/rextendr', lib='.pixi/envs/default/lib/R/library/') "
+FILE=".pixi/.activated"
+if [ -f $FILE ]; then
+    echo "we are active"
+else
+    echo "installing extendr"
+    R -q -e " remotes::install_github('extendr/rextendr', lib='.pixi/envs/default/lib/R/library/') "
+    touch $FILE
+fi
