3b12790d26dd057b1108d8ff5c31d263ff9c706e
Sibbe Bakker <sibbe.bakker@wur.nl>
2024-12-14 15:47:00 +0100

    allow the usage of bioconductor things

--- DEVELOPING.md
+++ DEVELOPING.md
@@ -1,6 +1,6 @@
 # Developing
 
-For package development and testing, use the `pixi` environment.
+For package development and testing, use the `pixi` environment. See `pixi run help` for more information.
 
 
 Edit just the `README.Rmd`. Convert to README.md using `pixi run make-readme`.
--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,28 @@
 use extendr_api::prelude::*;
+use asap::core::utils::distances;
 
 /// Return string `"Hello world!"` to R.
 /// @export
 #[extendr]
 fn hello_world() -> &'static str {
+    let a = b"ATGCTCGTAGCTGCTACGTC";
+    let b = b"ATCCTCGAAGCAGCTACGAC";
+    let d =
+        distances::percentage_difference(a.as_ref(), b.as_ref()).unwrap();
+
+    assert!(d == 0.20);
+
     "Hello world!"
+}
+
+/// Return the p distance.
+/// @export
+#[extendr]
+fn hello_distance() -> f64 {
+    let a = b"ATGCTCGTAGCTGCTACGCA";
+    let b = b"ATCCTCGAAGCAGCTACGAC";
+    let d = distances::percentage_difference(a.as_ref(), b.as_ref()).unwrap();
+    d
 }
 
 // Macro to generate exports.
